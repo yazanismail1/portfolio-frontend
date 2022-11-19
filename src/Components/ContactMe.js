e7d250ed345d67b3a79afe8ada275887d488a543
yazanismail1 <yalfarra@outlook.com>
2022-11-19 22:16:01 +0000

    feat: added the contact me functionality

--- src/Components/ContactMe.js
+++ src/Components/ContactMe.js
@@ -1,27 +1,50 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import Header from './Header';
 import "../Styles/ContactMe.css";
+import emailjs from '@emailjs/browser';
+import swal from "sweetalert";
 
-class ContactMe extends React.Component{
+
+export const ContactMe = () => {
+
+  const form = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm("service_25zzwds", "template_binzxy8", form.current, "8NXhqFjUdTkZ_T9Ht")
+    .then((result) => {
+      console.log(result.text);
+  }, (error) => {
+      console.log(error.text);
+  });
+
+  e.target.reset();
+      swal({
+        title: "Email was sent successfully ! ",
+        icon: "success",
+        button: "Continue",
+        className: "swalButton"
+      });
+  }
     
-  render(){
     return(
       <>
       <Header />
       <div className='contactPage'>
-      <form className='contactForm'>
+      <form className='contactForm' ref = {form} onSubmit={handleSubmit}>
       <label>E-mail</label><br></br>
-      <input type="text" id="fname" name="fname" ></input><br></br>
+      <input type="email" id="email" name="email" ></input><br></br>
       <label>Subject</label><br></br>
-      <input type="text" id="fname" name="fname" ></input><br></br>
+      <input type="text" id="subject" name="subject" ></input><br></br>
       <label>Message</label><br></br>
-      <textarea rows="5" cols="50"></textarea><br></br>
-      <input type='submit'></input>
+      <textarea rows="5" cols="50" name="message"></textarea><br></br>
+      <input type='submit' value="Send"></input>
       </form>
     </div>
     </>
     )
-  }
+
 }
 
 export default ContactMe;
